0da3adf84342fbb58249cdb885c3c0366c5dcf3b
Bart Trojanowski <bart@jukie.net>
2023-02-20 08:40:20 -0500

    coroutines with CppInsights with coroutine expansion

--- cpp-coroutines/co-generator-insights.cpp
+++ cpp-coroutines/co-generator-insights.cpp
@@ -1,4 +1,10 @@
-/* using CppInsights on https://godbolt.org/z/6WjGcn */
+/* using CppInsights + coroutine transformations on https://godbolt.org/z/6WjGcn */
+
+/*************************************************************************************
+ * NOTE: The coroutine transformation you've enabled is a hand coded transformation! *
+ *       Most of it is _not_ present in the AST. What you see is an approximation.   *
+ *************************************************************************************/
+// infiniteDataStream.cpp
 
 #include <coroutine>
 #include <memory>
@@ -180,22 +186,135 @@ struct Generator<int>
   struct promise_type;
 };
 
-#endif
 
+
+struct __getNextFrame
+{
+  void (*resume_fn)(__getNextFrame *);
+  void (*destroy_fn)(__getNextFrame *);
+  std::__coroutine_traits_impl<Generator<int> >::promise_type __promise;
+  int __suspend_index;
+  bool __initial_await_suspend_called;
+  int start;
+  int step;
+  int value;
+  int i;
+  std::suspend_always __suspend_74_16;
+  std::suspend_always __suspend_79_9;
+  std::suspend_always __suspend_74_16_1;
+};
 
 Generator<int> getNext(int start, int step)
 {
-  std::operator<<(std::cout, "    getNext: start").operator<<(std::endl);
-  int value = start;
-  for(int i = 0; ; ++i) {
-    std::operator<<(std::cout, "    getNext: before co_yield").operator<<(std::endl);
-    co_yield value;
-    std::operator<<(std::cout, "    getNext: after co_yield").operator<<(std::endl);
-    value = static_cast<int>(value + step);
-  }
+  /* Allocate the frame including the promise */
+  __getNextFrame * __f = reinterpret_cast<__getNextFrame *>(operator new(__builtin_coro_size()));
+  __f->__suspend_index = 0;
+  __f->__initial_await_suspend_called = false;
+  __f->start = std::forward<int>(start);
+  __f->step = std::forward<int>(step);
   
+  /* Construct the promise. */
+  new (&__f->__promise)std::__coroutine_traits_impl<Generator<int> >::promise_type{};
+  
+  /* Forward declare the resume and destroy function. */
+  void __getNextResume(__getNextFrame * __f);
+  void __getNextDestroy(__getNextFrame * __f);
+  
+  /* Assign the resume and destroy function pointers. */
+  __f->resume_fn = &__getNextResume;
+  __f->destroy_fn = &__getNextDestroy;
+  
+  /* Call the made up function with the coroutine body for initial suspend.
+     This function will be called subsequently by coroutine_handle<>::resume()
+     which calls __builtin_coro_resume(__handle_) */
+  __getNextResume(__f);
+  
+  
+  return __promise.get_return_object();
 }
 
+/* This function invoked by coroutine_handle<>::resume() */
+void __getNextResume(__getNextFrame * __f)
+{
+  try 
+  {
+    /* Create a switch to get to the correct resume point */
+    switch(__f->__suspend_index) {
+      case 0: break;
+      case 1: goto __resume_getNext_1;
+      case 2: goto __resume_getNext_2;
+    }
+    
+    /* co_await insights.cpp:74 */
+    __f->__suspend_74_16 = __f->__promise.initial_suspend();
+    if(!__f->__suspend_74_16.await_ready()) {
+      __f->__suspend_74_16.await_suspend(std::coroutine_handle<Generator<int>::promise_type>::from_address(static_cast<void *>(__f)).operator coroutine_handle());
+      __f->__suspend_index = 1;
+      __f->__initial_await_suspend_called = true;
+      return;
+    } 
+    
+    __resume_getNext_1:
+    __f->__suspend_74_16.await_resume();
+    std::operator<<(std::cout, "    getNext: start").operator<<(std::endl);
+    __f->value = __f->start;
+    for( __f->i = 0; ; ++__f->i) {
+      std::operator<<(std::cout, "    getNext: before co_yield").operator<<(std::endl);
+      
+      /* co_yield insights.cpp:79 */
+      __f->__suspend_79_9 = __f->__promise.yield_value(__f->value);
+      if(!__f->__suspend_79_9.await_ready()) {
+        __f->__suspend_79_9.await_suspend(std::coroutine_handle<Generator<int>::promise_type>::from_address(static_cast<void *>(__f)).operator coroutine_handle());
+        __f->__suspend_index = 2;
+        return;
+      } 
+      
+      __resume_getNext_2:
+      __f->__suspend_79_9.await_resume();
+      std::operator<<(std::cout, "    getNext: after co_yield").operator<<(std::endl);
+      __f->value = static_cast<int>(__f->value + __f->step);
+    }
+    
+    goto __final_suspend;
+  } catch(...) {
+    if(!__f->__initial_await_suspend_called) {
+      throw ;
+    } 
+    
+    __f->__promise.unhandled_exception();
+  }
+  
+  __final_suspend:
+  
+  /* co_await insights.cpp:74 */
+  __f->__suspend_74_16_1 = __f->__promise.final_suspend();
+  if(!__f->__suspend_74_16_1.await_ready()) {
+    __f->__suspend_74_16_1.await_suspend(std::coroutine_handle<Generator<int>::promise_type>::from_address(static_cast<void *>(__f)).operator coroutine_handle());
+  } 
+  
+  ;
+}
+
+/* This function invoked by coroutine_handle<>::destroy() */
+void __getNextDestroy(__getNextFrame * __f)
+{
+  /* destroy all variables with dtors */
+  __f->~__getNextFrame();
+  /* Deallocating the coroutine frame */
+  operator delete(__builtin_coro_free(static_cast<void *>(__f)));
+}
+#endif
+
+Generator<int> getNext(int start = 10, int step = 10) {
+    std::cout << "    getNext: start" << std::endl;
+    auto value = start;
+    for (int i = 0;; ++i){
+        std::cout << "    getNext: before co_yield" << std::endl;
+        co_yield value;
+        std::cout << "    getNext: after co_yield" << std::endl;
+        value += step;
+    }
+}
 
 int main()
 {
