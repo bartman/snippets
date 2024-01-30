ae230f5ba75ccb291bee9554fa54d7d93c703bb6
Bart Trojanowski <bart@jukie.net>
2024-01-30 15:21:30 -0500

    cppmain: playing around

--- cpp-main/main.cpp
+++ cpp-main/main.cpp
@@ -1,15 +1,26 @@
-#include <string_view>
-#include <vector>
 #include <iostream>
+#include <string_view>
+#include <thread>
+#include <vector>
 
-int main(const int argc, const char* const* const argv)
+int foo(int i)
 {
-	std::vector<std::string_view> args(argv,
-		std::next(argv, static_cast<std::ptrdiff_t>(argc)));
+    return 0;
+}
 
-	for (auto &arg : args) {
-		std::cout << arg << std::endl;
-	}
+int main(const int argc, const char *const *const argv)
+{
+    std::vector<std::string_view> args(
+        argv, std::next(argv, static_cast<std::ptrdiff_t>(argc)));
 
-	return 0;
+    std::cout << "args=" << args.size() << std::endl;
+
+    for (const auto &arg : args) {
+        std::cout << arg << std::endl;
+    }
+
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(10s);
+
+    return 0;
 }
